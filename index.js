11f1a996ae2562c2d9958d43020df05f8b9a2617
aafrinshahas <codergit6@gmail.com>
2024-04-24 14:20:09 +0530

    modidy js code

--- index.js
+++ index.js
@@ -3,16 +3,25 @@ var container = document.querySelector('.container');
 async function fetchData(){
     const res = await fetch('https://www.boredapi.com/api/activity', {method: "GET"})
    const data = await res.json()
-//   console.log(data.activity)
-container.innerHTML += `
-<h1>${data.activity}</h1>
-<p>Type: ${data.type}</p>
-<p>Participants: ${data.participants}</p>
-<p>Price: ${data.price}</p>
-<p>Key: ${data.key}</p>
-<p>Accessibility: ${data.accessibility}</p>
-<p>Link: ${data.link}</p>
-`
+
+const dataArr = [];
+dataArr.push(data)
+console.log(dataArr)
+
+dataArr.map((item)=>{
+    return(
+        container.innerHTML += `
+        <h1>${item.activity}</h1>
+        <p>Type: ${item.type}</p>
+        <p>Participants: ${item.participants}</p>
+        <p>Price: ${item.price}</p>
+        <p>Key: ${item.key}</p>
+        <p>Accessibility: ${item.accessibility}</p>
+        <p>Link: ${item.link}</p>
+        `
+    )
+})
+
 //   const result = `
 // ${data.activity}
 //  Type: ${data.type}
